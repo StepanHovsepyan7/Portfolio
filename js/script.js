b6ed029091c49ec1546099f3112dde23c786025c
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2023-01-27 22:16:52 +0400

    mobile navigation

--- js/script.js
+++ js/script.js
@@ -1,2 +1,50 @@
 'use strict';
 
+AOS.init({
+    duration: 2000,
+    once: true,
+  });
+
+
+
+  let fabars = $('.fa-bars');
+  let menu = $('.mobileParent');
+  let faxmark = $('.fa-xmark');
+  let blackdiv = $('.blackdiv');
+
+
+  if($(window).width() < 1100) {
+
+
+    fabars.click(function () {
+        menu.css({
+            display: 'block',
+            display: 'flex',
+            justifyContent: 'end',
+        })
+
+        blackdiv.css({
+            display: 'block',
+        })
+
+    })  
+    
+    blackdiv.click(function () {
+        menu.css({
+            display: 'none',
+        })
+
+        blackdiv.css({
+            display: 'none',
+        })
+    })
+
+
+    faxmark.click(function(){
+        menu.css({
+            display: 'none',
+        })
+    })
+
+
+}
